824585fdf2be85cdda95f9c11b87d16d2a805887
wonjibkim <101384372+wonjibkim@users.noreply.github.com>
2022-09-23 22:53:04 +0900

    09/23 백준

---
+++ src/BAK2557.java
@@ -0,0 +1,6 @@
+public class BAK2557 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/Bak10718.java
@@ -0,0 +1,7 @@
+public class Bak10718 {
+    public static void main(String[] args) {
+        System.out.println("강한친구 육군");
+        System.out.println("강한친구 육군");
+    }
+
+}
