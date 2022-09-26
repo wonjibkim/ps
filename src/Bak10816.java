32e3c9d549c2523579030bf62fed6bec211064e9
wonjibkim <101384372+wonjibkim@users.noreply.github.com>
2022-09-26 21:33:37 +0900

    9/26 백준 선택정렬

---
+++ src/Bak10816.java
@@ -0,0 +1,2 @@
+public class Bak10816 {
+}
---
+++ src/Bak18108.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Bak18108 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+      int bc = sc.nextInt();
+
+        System.out.println(bc - 543);
+
+
+    }
+}
