880af7b9663f3bd0b8457d43c1706dc1253b034b
wonjibkim <101384372+wonjibkim@users.noreply.github.com>
2022-10-19 09:13:15 +0900

    10/18 백준

---
+++ src/Bak2775.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Bak2775 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+
+        int[][] APT = new int[15][15];
+
+        for(int i = 0; i < 15; i++) {
+            APT[i][1] = 1;
+            APT[0][i] = i;
+        }
+
+
+        for(int i = 1; i < 15; i ++) {
+
+            for(int j = 2; j < 15; j++) {
+                APT[i][j] = APT[i][j - 1] + APT[i - 1][j];
+            }
+        }
+
+
+        int T = in.nextInt();
+
+        for(int i = 0; i < T; i++) {
+            int k = in.nextInt();
+            int n = in.nextInt();
+            System.out.println(APT[k][n]);
+        }
+    }
+
+}
+
