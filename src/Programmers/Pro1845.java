53bc0d9a06a9abd7f7d07cd23e28d7bd86448599
wonjibkim <101384372+wonjibkim@users.noreply.github.com>
2022-10-31 18:57:35 +0900

    10/29 포켓몬

--- src/Programmers/Pro1845.java
+++ src/Programmers/Pro1845.java
@@ -1,28 +1,31 @@
 package Programmers;
 
-import java.util.Scanner;
+import java.sql.SQLOutput;
+import java.util.*;
 
 public class Pro1845 {
     public static void main(String[] args) {
 
+        int[] nums = {3,3,3,2,2,2};
+        int answer = 0;
 
 
-        int[] porket = {3,1,2,3};
-
-        String[]tepArr = new String[porket.length];
-        
-        int j = 0;
+        Set<Integer> set = new HashSet<Integer>();
 
 
+        for (int i = 0; i < nums.length ; i++) {
+            set.add(nums[i]);
 
+        }
+        if (nums.length/2 < set.size()){
 
+            answer = nums.length/2;
+        }else {
+            answer = set.size();
+        }
 
-
-
-
-
-
-
-
+        System.out.println(answer);
     }
+
 }
+
